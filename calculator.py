23c646916456009a5a6c6104f8f406b851810941
Basir <basir.askari@contractor.datalogic.com>
2026-05-21 16:03:16 +0200

    add new addetion

--- calculator.py
+++ calculator.py
@@ -26,3 +26,4 @@ if __name__ == "__main__":
     print(f"10 - 5  = {subtract(10, 5)}")
     print(f"10 * 5  = {multiply(10, 5)}")
     print(f"10 / 5  = {divide(10, 5)}")
+    print(f"23 + 12 = {add(23,12)}")
